8e29e77616b334ce74b5a75b771d0515d3acad0a
warmonger <d>
2015-01-29 18:16:18 +0600

    Fix for travis

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -10,14 +10,14 @@ class TestAll(unittest.TestCase):
     def test_times(self):
         human_time = '2015-01-01 18:21:26'
         datetime_time = datetime.datetime(2015, 1, 1, 18, 21, 26)
-        unix_time = 1420114886
+        my_unix_time = 1420114886
 
-        self.assertEqual(d2u(datetime_time), unix_time)
+        self.assertEqual(d2u(datetime_time), my_unix_time)
         self.assertEqual(d2h(datetime_time), human_time)
-        self.assertEqual(h2u(human_time), unix_time)
+        self.assertEqual(h2u(human_time), my_unix_time)
         self.assertEqual(h2d(human_time), datetime_time)
-        self.assertEqual(u2d(unix_time), datetime_time)
-        self.assertEqual(u2h(unix_time), human_time)
+        self.assertEqual(u2d(my_unix_time), datetime_time)
+        self.assertEqual(u2h(my_unix_time), human_time)
 
 
 if __name__ == '__main__':
